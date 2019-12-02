1a7c0b75847c834c98d129f9711c4adc205c7f67
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-12-02 11:39:28 -0800

    completed handle weather functionality

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.static('front-end'))
 
+// Routes
+app.get('/location', handleLocation)
+app.get('/weather', handleWeather)
+
 // Contructors
 function Location(city, geoData) {
   this.search_query = city;
@@ -24,9 +28,12 @@ function Location(city, geoData) {
   this.longitude = geoData.results[0].geometry.location.lng;
 }
 
-// Routes
-app.get('/location', handleLocation)
+function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toDateString();
+}
 
+// Fetch Requests
 function handleLocation(request,response) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
@@ -39,6 +46,24 @@ function handleLocation(request,response) {
     .catch(error => {
       response.status(500).send('Status: 500. Sorry, there is something not quite right');
     })
+}
+
+function handleWeather(request, response) {
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+
+  superagent.get(url)
+    .then( data => {
+      const weatherSummaries = data.body.daily.data.map(day => new Weather(day));
+      response.status(200).send(weatherSummaries);
+    })
+    .catch( error => {
+      errorHandler('So sorry, something went really wrong', request, response);
+    });
+}
+
+// Error Handler function to throw
+function errorHandler(error,request,response) {
+  response.status(500).send(error);
 }
 
 // Error if route does not exist
