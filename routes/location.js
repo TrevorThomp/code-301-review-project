4ec6a314c07fbd127e003f0858eef9362018d7f6
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-12-03 11:43:55 -0800

    completed saving location data to database

--- routes/location.js
+++ routes/location.js
@@ -8,7 +8,6 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('err', err => console.error(err));
 
-// Location Constructor
 function Location(query, data){
   this.search_query = query;
   this.formatted_query = data.formatted_address;
@@ -16,42 +15,8 @@ function Location(query, data){
   this.longitude = data.geometry.location.lng;
 }
 
-// Google Geocode API Fetch
-Location.fetchLocation = function (query){
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
-
-  return superagent
-    .get(url)
-    .then( result=> {
-      if(!result.body.results.length) {throw 'No data';}
-      let location = new Location(query, result.body.results[0]);
-      return location.save()
-        .then( result => {
-          location.id = result.rows[0].id; //update, delete...etc...
-          return location;
-        });
-    });
-};
-
-// Location SQL lookup
-Location.lookup = (handler) => {
-  const SQL = `SELECT * FROM locations WHERE search_query=$1`;
-  const values = [handler.query];
-
-  return client.query(SQL, values)
-    .then( results => {
-      if (results.rowCount > 0){
-        handler.cacheHit(results);
-      }else {
-        handler.cacheMiss();
-      }
-    })
-    .catch(console.error);
-};
-
-// Location SQL save data
 Location.prototype.save = function(){
-  const SQL = `INSERT INTO locations
+  const SQL = `INSERT INTO location
   (search_query, formatted_query, latitude, longitude)
   VALUES ($1, $2, $3, $4)
   RETURNING *`;
@@ -60,26 +25,21 @@ Location.prototype.save = function(){
   return client.query(SQL, values);
 };
 
-// Function to query DB or fetch from API
-function getLocation(request,response) {
+router.get('/', function(request,response) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
-  const locationHandler = {
-    query: request.query.data,
+  const query = request.query.data;
 
-    cacheHit: (results) => {
-      console.log('Got data from DB');
-      response.send(results.rows[0]);
-    },
-
-    cacheMiss: () => {
-      console.log('No data in DB, fetching...');
-      Location.fetchLocation(request.query.data)
-        .then( data => response.send(data));
-    }
-  };
-  Location.lookup(locationHandler);
-}
-
-router.get('/', getLocation)
+  return superagent.get(url)
+    .then( data => {
+      const location = new Location(query, data.body.results[0])
+      location.save()
+      return location
+    })
+    .then(data => response.send(data))
+    .catch( () => {
+      response.status(500).send('Status: 500. Sorry, there is something not quite right');
+    })
+})
 
 module.exports = router;
