651f4a7086b3b2bc944f5c5edc93ee73a67bb708
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-12-03 08:45:38 -0800

    started saving location to DB

--- routes/location.js
+++ routes/location.js
@@ -8,76 +8,78 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('err', err => console.error(err));
 
+// Location Constructor
+function Location(query, data){
+  this.search_query = query;
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
+}
 
-router.get('/', function(request,response){
+// Google Geocode API Fetch
+Location.fetchLocation = function (query){
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
 
-  // Location Constructor
-  function Location(city, geoData) {
-    this.search_query = city;
-    this.formatted_query = geoData.results[0].formatted_address;
-    this.latitude = geoData.results[0].geometry.location.lat;
-    this.longitude = geoData.results[0].geometry.location.lng;
-  }
+  return superagent
+    .get(url)
+    .then( result=> {
+      if(!result.body.results.length) {throw 'No data';}
+      let location = new Location(query, result.body.results[0]);
+      return location.save()
+        .then( result => {
+          location.id = result.rows[0].id; //update, delete...etc...
+          return location;
+        });
+    });
+};
 
-  Location.prototype.save = function(){
-    const SQL = `INSERT INTO location (search_query, formatted_query, latitude, longitude VALUES ($1, $2, $3, $4) RETURNING *`;
+// Location SQL lookup
+Location.lookup = (handler) => {
+  const SQL = `SELECT * FROM locations WHERE search_query=$1`;
+  const values = [handler.query];
 
-    let values = Object.values(this);
-    return client.query(SQL, values);
-  }
-
-  // API Fetch
-  Location.getLocation = function (query) {
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
-
-    return superagent.get(url)
-      .then(data => {
-        let location = new Location(query, data.body.results[0]);
-        return location.save()
-          .then(result => {
-            location.id = result.rows[0].id;
-            return location
-          })
-      })
-      .catch(error => {
-        response.status(500).send('Status: 500. Sorry, there is something not quite right');
-      })
-  }
-
-  // Location SQL lookup
-  Location.lookup = (handler) => {
-    const SQL = `SELECT * FROM locations WHERE search_query=$1`;
-    const values = [handler.query];
-
-    return client.query(SQL, values)
-      .then( results => {
-        if (results.rowCount > 0){
-          handler.cacheHit(results);
-        }else {
-          handler.cacheMiss();
-        }
-      })
-      .catch(console.error);
-  };
-
-  function fetchLocation(request,response) {
-
-    const locationHandler = {
-      query: request.query.data,
-
-      cacheHit: (results) => {
-        console.log('Got data from DB');
-        response.send(results.rows[0]);
-      },
-
-      cacheMiss: () => {
-        console.log('No data in DB, fetching...');
-        Location.fetchLocation(request.query.data)
-          .then( data => response.send(data));
+  return client.query(SQL, values)
+    .then( results => {
+      if (results.rowCount > 0){
+        handler.cacheHit(results);
+      }else {
+        handler.cacheMiss();
       }
-    };
-    Location.lookup(locationHandler);
-  }
-})
+    })
+    .catch(console.error);
+};
+
+// Location SQL save data
+Location.prototype.save = function(){
+  const SQL = `INSERT INTO locations
+  (search_query, formatted_query, latitude, longitude)
+  VALUES ($1, $2, $3, $4)
+  RETURNING *`;
+
+  let values = Object.values(this);
+  return client.query(SQL, values);
+};
+
+// Function to query DB or fetch from API
+function getLocation(request,response) {
+
+  const locationHandler = {
+    query: request.query.data,
+
+    cacheHit: (results) => {
+      console.log('Got data from DB');
+      response.send(results.rows[0]);
+    },
+
+    cacheMiss: () => {
+      console.log('No data in DB, fetching...');
+      Location.fetchLocation(request.query.data)
+        .then( data => response.send(data));
+    }
+  };
+  Location.lookup(locationHandler);
+}
+
+router.get('/', getLocation)
 
 module.exports = router;
