4406051b7e46a142496b89d993383932f08984ba
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-12-04 10:59:24 -0800

    started queryMovie functionality

--- routes/movies.js
+++ routes/movies.js
@@ -35,3 +35,37 @@ Movies.prototype.getMovie = function(location) {
       return movieData;
     })
 }
+
+Movies.prototype.save = function(locationID) {
+  const SQL = `INSERT INTO movies (title, overview, average_votes, total_votes, image_url, popularity, released_on, location_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
+  const values = Object.values(this);
+  values.push(locationID)
+
+  return client.query(SQL, values)
+}
+
+Movies.prototype.lookup = function(table) {
+  const SQL = `SELECT * FROM movies WHERE location_id=$1`;
+  const values = [table.location.id]
+
+  return client.query(SQL, values)
+    .then(results => {
+      if (results.rowCount > 0) {
+        table.dataHit(results)
+      } else {
+        table.dataMiss()
+      }
+    })
+    .catch(err => console.error(err))
+}
+
+function queryMovies(request,response) {
+  const movies = {
+    location: request.query.data,
+
+    dataHit: (results) => {
+      console.log('got movies from DB')
+      
+    }
+  }
+}
