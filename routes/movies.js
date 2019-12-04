2d1da426e97e900d3cbb38379dcddb6b8e3ffde3
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-12-04 10:46:15 -0800

    completed movie constructor

---
+++ routes/movies.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('dotenv').config();
+
+// Application Dependencies
+const superagent = require('superagent');
+const pg = require('pg');
+
+// Connects to PSQL
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('err', err => console.error(err));
